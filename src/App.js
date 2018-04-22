38f2fb5cf4f057fa838f3e262a67bf591715b3b6
Christopher Miller <admin@mctherealm.net>
2018-04-22 11:59:36 -0400

    Revamped website to use material UI

--- src/App.js
+++ src/App.js
@@ -1,68 +1,109 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { Header, Button, Segment, Icon, Container } from 'semantic-ui-react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import {Card, CardTitle, CardActions, CardText} from 'material-ui/Card';
+import Chip from 'material-ui/Chip';
+import {blue300, indigo300, red400, grey500, yellow600} from 'material-ui/styles/colors';
+import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
 
-import Particles from 'react-particles-js';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Particles
-          className="particles"
-          params={{
-            particles: {
-              color: {
-                value: '#000000'
-              },
-              number: {
-                value: 100
-              },
-              line_linked: {
-                shadow: {
-                  enable: true,
-                  color: "#000000",
-                  blur: 5
-                }
-              }
-            }
-          }}
-        />
-        <Header as='h1' content={"Hi, I'm Chris"} subheader={"I Make Things With Code"} textAlign={"center"}/>
-        <Segment basic textAlign="center">
-          <Button color='github' as='a' href='https://github.com/ChristopherJMiller'>
-            <Icon name='github' /> ChristopherJMiller
-          </Button>
-          <Button color='twitter' as='a' href='https://twitter.com/Chris7332'>
-            <Icon name='twitter' /> @Chris7332
-          </Button>
-        </Segment>
-        <Header as='h2' content={"Projects"} textAlign={"center"}/>
-        <Segment basic textAlign="center">
-          <Button color='grey' as='a' href='https://github.com/ChristopherJMiller/doggo-factory'>
-            Doggo Factory
-          </Button>
-          <Button color='grey' as='a' href='https://git.mctherealm.net/Froger/lakewood-leaders'>
-            Lakewood Leaders Website
-          </Button>
-          <Button color='grey' as='a' href='https://git.mctherealm.net/Froger/lakewoodranchtsa'>
-            LRHS TSA Website
-          </Button>
-        </Segment>
-        <Container as='h4' textAlign={"center"}><a href="http://tsaweb.org"><Header as='h4' content={"Projects Through TSA"} textAlign={"center"} /> Learn More</a></Container>
-        <Segment basic textAlign="center">
-          <Button as='a' href='https://git.mctherealm.net/LRHS-TSA/teammate/src/develop'>
-            TeamMate
-          </Button>
-          <Button as='a' href='https://git.mctherealm.net/LRHS-TSA/testor-server'>
-            Testor Server
-          </Button>
-          <Button as='a' href='https://git.mctherealm.net/LRHS-TSA/Galaquest'>
-            Galaquest
-          </Button>
-        </Segment>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <AppBar
+            title="Chris Miller"
+            showMenuIconButton={false}
+            zDepth={2}
+            style={{position: 'fixed'}}
+          />
+          <div className="row center-md">
+            <Card className="col-md-8 col-sm-12 card-margins">
+              <CardTitle title="Hi, I'm Chris" subtitle="I Make Things with Code" />
+              <CardText>
+                My focus is on modern web development, robotics, and game development. 
+              </CardText>
+            </Card>
+
+            <h1 className="col-md-8 card-margins-small">Current Projects</h1>
+
+            <Card className="col-md-8 col-sm-12 card-margins">
+              <CardTitle title="Lakewood Leaders Website" subtitle="Community Service Management" />
+              <CardText>
+                Lakewood Leaders is a community service organization at Lakewood Ranch Highschool. The website manages members and allows for electronic event sign up.
+                <CardActions>
+                  <RaisedButton
+                    href="https://github.com/ChristopherJMiller/lakewood-leaders"
+                    target="_blank"
+                    label="Github Link"
+                    secondary={false}
+                  />
+                  <RaisedButton
+                    href="https://leaders.lrhsclubs.com/"
+                    target="_blank"
+                    label="Website"
+                    secondary={true}
+                  />
+                </CardActions>
+                <CardActions className="chip-wrap">
+                  <Chip backgroundColor={red400} className="chip">
+                    Ruby on Rails
+                  </Chip>
+
+                  <Chip backgroundColor={blue300} className="chip">
+                    Docker
+                  </Chip>
+
+                  <Chip backgroundColor={indigo300} className="chip">
+                    Kubernetes
+                  </Chip>
+                </CardActions>
+              </CardText>
+            </Card>
+
+            <Card className="col-md-8 col-sm-12 card-margins">
+              <CardTitle title="RhyVR" subtitle="VR Drum Rhythm Game" />
+              <CardText>
+                RhyVR will bring stepmania mechanics and open source feel to Virtual Reality. The game is still within prototype stages.
+                <CardActions>
+                  <RaisedButton
+                    href="https://github.com/ChristopherJMiller/rhyvr"
+                    target="_blank"
+                    label="Github Link"
+                    secondary={false}
+                  />
+                  <RaisedButton
+                    href="https://github.com/ChristopherJMiller/rhyvr-song-editor"
+                    target="_blank"
+                    label="Song Editor Github Link"
+                    secondary={false}
+                  />
+                  <RaisedButton
+                    href="https://www.youtube.com/watch?v=Ig1xqisP1hs"
+                    target="_blank"
+                    label="Prototype Video"
+                    secondary={true}
+                  />
+                </CardActions>
+                <CardActions className="chip-wrap">
+                  <Chip backgroundColor={grey500} className="chip">
+                    Unity3D
+                  </Chip>
+
+                  <Chip backgroundColor={yellow600} className="chip">
+                    Electron
+                  </Chip>
+                </CardActions>
+              </CardText>
+            </Card>
+          </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import '../node_modules/flexboxgrid/css/flexboxgrid.min.css'
 
 import 'semantic-ui-css/semantic.min.css';
 

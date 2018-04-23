5842381b99ba5e57599822564392124cdfbdd306
Christopher Miller <admin@mctherealm.net>
2018-04-23 19:19:06 -0400

    Added more cards and social media menu

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import {Card, CardTitle, CardActions, CardText} from 'material-ui/Card';
 import Chip from 'material-ui/Chip';
-import {blue300, indigo300, red400, grey500, yellow600} from 'material-ui/styles/colors';
+import {purple100, blue300, indigo300, cyan700, red400, grey500, yellow600, blueGrey200} from 'material-ui/styles/colors';
 import RaisedButton from 'material-ui/RaisedButton';
-
-
+import FontIcon from 'material-ui/FontIcon';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 class App extends Component {
   render() {
@@ -19,7 +22,21 @@ class App extends Component {
             title="Chris Miller"
             showMenuIconButton={false}
             zDepth={2}
-            style={{position: 'fixed'}}
+            style={{position: 'fixed', backgroundColor: cyan700 }}
+            iconElementRight={
+              <IconMenu
+                iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+                targetOrigin={{horizontal: 'left', vertical: 'top'}}
+
+                onItemClick={(event, menuItem, index) => {
+                  window.open(menuItem.props.value, '_blank');
+                }}
+              >
+                <MenuItem primaryText="Github" value="https://github.com/ChristopherJMiller" leftIcon={<FontIcon className="fa fa-github" />} />
+                <MenuItem primaryText="Twitter" value="https://twitter.com/Chris7332" leftIcon={<FontIcon className="fa fa-twitter" />} />
+              </IconMenu>
+            }
           />
           <div className="row center-xs">
             <Card className="col-md-8 col-sm-12 card-margins">
@@ -31,7 +48,7 @@ class App extends Component {
 
             <h1 className="col-md-8 card-margins-small">Current Projects</h1>
 
-            <Card className="col-md-8 col-sm-12 card-margins">
+            <Card className="col-md-5 col-sm-12 card-margins-small card-group">
               <CardTitle title="Lakewood Leaders Website" subtitle="Community Service Management" />
               <CardText>
                 Lakewood Leaders is a community service organization at Lakewood Ranch Highschool. The website manages members and allows for electronic event sign up.
@@ -65,7 +82,7 @@ class App extends Component {
               </CardText>
             </Card>
 
-            <Card className="col-md-8 col-sm-12 card-margins">
+            <Card className="col-md-5 col-sm-12 card-margins-small card-group">
               <CardTitle title="RhyVR" subtitle="VR Drum Rhythm Game" />
               <CardText>
                 RhyVR will bring stepmania mechanics and open source feel to Virtual Reality. The game is still within prototype stages.
@@ -73,13 +90,13 @@ class App extends Component {
                   <RaisedButton
                     href="https://github.com/ChristopherJMiller/rhyvr"
                     target="_blank"
-                    label="Github Link"
+                    label="Github"
                     secondary={false}
                   />
                   <RaisedButton
                     href="https://github.com/ChristopherJMiller/rhyvr-song-editor"
                     target="_blank"
-                    label="Song Editor Github Link"
+                    label="Song Editor Github"
                     secondary={false}
                   />
                   <RaisedButton
@@ -96,6 +113,54 @@ class App extends Component {
 
                   <Chip backgroundColor={yellow600} className="chip">
                     Electron
+                  </Chip>
+                </CardActions>
+              </CardText>
+            </Card>
+
+            <h1 className="col-md-8 card-margins-small">Past Projects</h1>
+
+            <Card className="col-md-5 col-sm-12 card-margins-small card-group">
+              <CardTitle title="Doggo Factory" subtitle="Bone Tycoon Idling Game" />
+              <CardText>
+                A quick little idling game about a dog's bone making tycoon.
+                <CardActions>
+                  <RaisedButton
+                    href="https://github.com/ChristopherJMiller/doggo-factory"
+                    target="_blank"
+                    label="Github"
+                    secondary={false}
+                  />
+                  <RaisedButton
+                    href="http://chrismiller.xyz/doggo-factory"
+                    target="_blank"
+                    label="Website"
+                    secondary={true}
+                  />
+                </CardActions>
+                <CardActions className="chip-wrap">
+                  <Chip backgroundColor={purple100} className="chip">
+                    React
+                  </Chip>
+                </CardActions>
+              </CardText>
+            </Card>
+
+            <Card className="col-md-5 col-sm-12 card-margins-small card-group">
+              <CardTitle title="Corgi Loaf" subtitle="I had an extra domain coupon." />
+              <CardText>
+                Wanted to contribute my own filler website.
+                <CardActions>
+                  <RaisedButton
+                    href="http://corgiloaf.net/"
+                    target="_blank"
+                    label="Website"
+                    secondary={true}
+                  />
+                </CardActions>
+                <CardActions className="chip-wrap">
+                  <Chip backgroundColor={blueGrey200} className="chip">
+                    Memes
                   </Chip>
                 </CardActions>
               </CardText>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import '../node_modules/flexboxgrid/css/flexboxgrid.min.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
 
 import 'semantic-ui-css/semantic.min.css';
 
